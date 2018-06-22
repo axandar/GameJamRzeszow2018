df0c56a8c45f3e2f898ca792682ad22ac4ed50a6
SuperStarPL <polskafilms@gmail.com>
2018-06-22 17:32:02 +0200

    moving road

---
+++ Assets/Scripts/MovingRoad.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingRoad : MonoBehaviour {
+
+    private SpriteRenderer _sr;
+    public float speed = 3f;
+
+	// Use this for initialization
+	void Start () {
+        _sr = _sr ?? GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector2 offset = _sr.material.GetTextureOffset("_MainTex");
+        offset.y += Time.deltaTime * speed;
+        _sr.material.SetTextureOffset("_MainTex", offset);
+	}
+}
