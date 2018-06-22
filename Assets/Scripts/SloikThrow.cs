5b41d77d490e12b41c448f846f508bd5e0a13022
SuperStarPL <polskafilms@gmail.com>
2018-06-23 01:04:58 +0200

    sloikthrowing

---
+++ Assets/Scripts/SloikThrow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SloikThrow : MonoBehaviour
+{
+    public GameObject sloikPrefab;
+
+    private Vector2 mousePos;
+
+    public int currSloik = 6;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && GameManager.instance.inGame)
+        {
+            mousePos = Input.mousePosition;
+            GameObject sloik = Instantiate(sloikPrefab, transform.position, Quaternion.identity);
+            sloik.GetComponent<Sloik>().SetTarget(Camera.main.ScreenToWorldPoint(mousePos));
+            sloik.GetComponent<Sloik>().SetType(currSloik);
+        }
+    }
+}
