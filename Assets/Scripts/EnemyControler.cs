e5ccd6ac75ded0b3c64cde073181952fb62c007f
Michał Piątkowski <axandar29@gmail.com>
2018-06-22 18:33:15 +0200

    Added EnemyController and prefabs

---
+++ Assets/Scripts/EnemyControler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControler : MonoBehaviour {
+    public float speed;
+    public int life;
+    public bool isDead = false;
+    public int lineNumber;
+    public GameObject roadLine;
+
+    private new Transform transform;
+    private int screenWidth;
+
+
+
+	// Use this for initialization
+	void Start () {
+        transform = GetComponent<Transform>();
+        //y
+        float yPosition = roadLine.transform.GetPositionY();
+        Debug.Log(yPosition);
+        Vector3 vec = new Vector3(1, yPosition, 1);
+        transform.Translate(vec);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 vec = new Vector3(speed, 0, 0);
+        transform.Translate(vec * Time.deltaTime);
+    }
+
+    void Kill() {
+        isDead = true;
+    }
+}
