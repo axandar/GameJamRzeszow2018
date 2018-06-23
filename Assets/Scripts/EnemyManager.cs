ddfe925f85d34d05a9a2476bcffdad0eb8a486e4
SuperStarPL <polskafilms@gmail.com>
2018-06-23 06:26:33 +0200

    new camera

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -15,10 +15,10 @@ public class EnemyManager : MonoBehaviour
 
     //spawns properties
     public float maxEnemyCooldown = 2f;
+
     public float minEnemyCooldown = 1f;
     public float maxKaretkaCooldown = 16f;
     public float minKaretkaCooldown = 10f;
-
 
     public float[] lanes = new float[] {
         5.25f,
@@ -28,73 +28,94 @@ public class EnemyManager : MonoBehaviour
         -3.25f
     };
 
-    public void SpawnKaretka(int lane){
-        if (lane > 5 || lane < 0) {
+    public void SpawnKaretka(int lane)
+    {
+        if (lane > 5 || lane < 0)
+        {
             return;
         }
-        GameObject e = Instantiate(karetkaCar, new Vector3(-15f, lanes[lane], 0f), Quaternion.identity);
+        GameObject e = Instantiate(karetkaCar, new Vector3(-25f, lanes[lane], 0f), Quaternion.identity);
     }
 
-    public void SpawnEnemy(int lane){
-        if (lane > 5 || lane < 0){
+    public void SpawnEnemy(int lane)
+    {
+        if (lane > 5 || lane < 0)
+        {
             return;
         }
 
         //todo ustawianie zmiennych w enemyPrefab
 
-        GameObject e = Instantiate(enemyPrefab, new Vector3(-15f, lanes[lane], 0f), Quaternion.identity);
+        GameObject e = Instantiate(enemyPrefab, new Vector3(-25f, lanes[lane], 0f), Quaternion.identity);
         spawnedEnemies.Add(e.GetComponent<EnemyControler>());
     }
 
-    private void Update(){
+    private void Update()
+    {
         WearOff();
 
         EnemySpawnCounter();
         KaretkaSpawnCounter();
     }
 
-    private void WearOff(){
-        if (duration > 0){
+    private void WearOff()
+    {
+        if (duration > 0)
+        {
             duration -= Time.deltaTime;
-        }else if (duration < 0){
+        }
+        else if (duration < 0)
+        {
             duration = 0;
             speedUpValue = 0;
         }
     }
 
-    private void EnemySpawnCounter() {
-        if(enemyCooldown > 0) {
+    private void EnemySpawnCounter()
+    {
+        if (enemyCooldown > 0)
+        {
             enemyCooldown -= Time.deltaTime;
-        } else if(enemyCooldown < 0) {
+        }
+        else if (enemyCooldown < 0)
+        {
             SpawnEnemy(DrawLineNumber());
             enemyCooldown = UnityEngine.Random.Range(minEnemyCooldown, maxEnemyCooldown);
         }
     }
 
-    private void KaretkaSpawnCounter() {
-        if(karetkaCooldown > 0) {
+    private void KaretkaSpawnCounter()
+    {
+        if (karetkaCooldown > 0)
+        {
             karetkaCooldown -= Time.deltaTime;
-        } else if(karetkaCooldown < 0) {
+        }
+        else if (karetkaCooldown < 0)
+        {
             SpawnKaretka(DrawLineNumber());
             karetkaCooldown = UnityEngine.Random.Range(minKaretkaCooldown, maxKaretkaCooldown);
         }
     }
 
-    private int DrawLineNumber() {
+    private int DrawLineNumber()
+    {
         return UnityEngine.Random.Range(0, 5);
     }
 
-    public void SpeedUpEnemies(int addedSpeedValue, float wearOffValue){
+    public void SpeedUpEnemies(int addedSpeedValue, float wearOffValue)
+    {
         speedUpValue = addedSpeedValue;
         duration = wearOffValue;
     }
 
-    public void RestoreEnemySpeed(){
+    public void RestoreEnemySpeed()
+    {
         speedUpValue = 0;
         duration = 0;
     }
 
-    public void RemoveEnemyFromList(EnemyControler enemyControler) {
+    public void RemoveEnemyFromList(EnemyControler enemyControler)
+    {
         spawnedEnemies.Remove(enemyControler);
     }
 }
