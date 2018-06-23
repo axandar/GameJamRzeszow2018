1fef70799a3ae3f81caeebceb9d3cdb868062488
SuperStarPL <polskafilms@gmail.com>
2018-06-23 02:33:32 +0200

    sounds

---
+++ Assets/Scripts/ObstacleManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleManager : MonoBehaviour
+{
+    public GameObject narrowRoadUp;     //0
+    public GameObject narrowRoadDown;   //1
+    public GameObject roadWorks;        //2
+
+    public float[] lanes = new float[] {
+        5.25f,
+        3.1f,
+        1f,
+        -1.1f,
+        -3.25f
+    };
+
+    public float minCooldownBetweenObstacles = 1f;
+    public float maxCooldownBetweenObstacles = 6f;
+
+    private float _cooldown;
+
+    private void Start()
+    {
+        _cooldown = 10f;
+    }
+
+    private void Update()
+    {
+        if (_cooldown > 0f)
+        {
+            _cooldown -= Time.deltaTime;
+        }
+        else
+        {
+            _cooldown = UnityEngine.Random.Range(minCooldownBetweenObstacles, maxCooldownBetweenObstacles);
+
+            SpawnRandomObstacle();
+        }
+    }
+
+    private void SpawnRandomObstacle()
+    {
+        int lane = UnityEngine.Random.Range(0, 4);
+
+        switch ((int)UnityEngine.Random.Range(0, 3))
+        {
+            case 0:
+                Instantiate(narrowRoadUp, new Vector3(25f, lanes[0]), Quaternion.identity);
+                break;
+
+            case 1:
+                Instantiate(narrowRoadDown, new Vector3(25f, lanes[4]), Quaternion.identity);
+                break;
+
+            case 2:
+                Instantiate(roadWorks, new Vector3(25f, lanes[lane]), Quaternion.identity);
+                break;
+        }
+    }
+}
